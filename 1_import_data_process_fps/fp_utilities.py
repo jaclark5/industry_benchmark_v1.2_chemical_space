1356e0c306ef9834d6de36d2091d4d336b5ebc86
jaclark5 <jennifer.clark@openforcefield.org>
2025-09-27 11:08:39 -0400

    Run creation of distance matrices

--- 1_import_data_process_fps/fp_utilities.py
+++ 1_import_data_process_fps/fp_utilities.py
@@ -1,10 +1,15 @@
 """Support functions to calculate molecular fingerprints using OpenEye Toolkits"""
 
+from typing import List, Tuple
 from openeye import oechem
 from openeye import oegraphsim
+from openeye.oegraphsim import OEFingerPrint
+
+# Suppress OpenEye warnings
+oechem.OEThrow.SetLevel(oechem.OEErrorLevel_Error)
 
 
-def get_fps(smiles_list: list, fptype: str = "Circular"):
+def get_fps(smiles_list: List[str], fptype: str = "Circular") -> List[OEFingerPrint]:
     """Compute OpenEye fingerprints for a list of molecules.
 
     Parameters
@@ -13,6 +18,11 @@ def get_fps(smiles_list: list, fptype: str = "Circular"):
         List of SMILES strings
     fptype : str, optional, default="Circular2"
         OpenEye fingerprint type, "Circular", "MACCS", or "LINGO"
+
+    Returns
+    -------
+    List[OEFingerPrint]
+        List of OpenEye fingerprints
     """
     fptype_dict = {
         "Circular": oegraphsim.OEMakeCircularFP,
@@ -34,3 +44,76 @@ def get_fps(smiles_list: list, fptype: str = "Circular"):
         fps.append(fp)
 
     return fps
+
+
+def get_distance_matrix(
+    fps: List[OEFingerPrint], metric: str = "Tanimoto"
+) -> Tuple[List[float], int]:
+    """
+    Generate a distance matrix (1 - similarity) for a list of fingerprints using the specified similarity metric.
+
+    Parameters
+    ----------
+    fps : list
+        List of OpenEye fingerprints
+    metric : str, optional, default="Tanimoto"
+        Similarity metric to use, one of "Tanimoto", "Dice", or "Cosine"
+
+    Returns
+    -------
+    distance_data : List[float]
+        The upper triangle distance matrix where the distance is ``1 - similarity``
+    n : int
+        The number of points (fingerprints) in the input list
+    """
+    metric_dict = {
+        "Tanimoto": oegraphsim.OETanimoto,
+        "Dice": oegraphsim.OEDice,
+        "Cosine": oegraphsim.OECosine,
+    }
+    if metric not in metric_dict:
+        raise ValueError(
+            f"Unsupported metric '{metric}'. Choose from {list(metric_dict.keys())}."
+        )
+
+    n = len(fps)
+    distance_data = []
+    for i in range(n):
+        for j in range(i):
+            sim = metric_dict[metric](fps[i], fps[j])
+            distance_data.append(1 - sim)
+    return distance_data, n
+
+
+def get_distance_vector(
+    fp: OEFingerPrint, fps: List[OEFingerPrint], metric: str = "Tanimoto"
+) -> List[float]:
+    """
+    Generate a distance vector (1 - similarity) for a fingerprint against a list of fingerprints using the specified similarity metric.
+    Supported metrics: "Tanimoto", "Dice", "Cosine"
+
+    Parameters
+    ----------
+    fp : OEFingerPrint
+        OpenEye fingerprint
+    fps : list[OEFingerPrint]
+        List of OpenEye fingerprints
+    metric : str, optional, default="Tanimoto"
+        Similarity metric to use, one of "Tanimoto", "Dice", or "Cosine"
+
+    Returns
+    -------
+    List[float]
+        Distance vector of the provided fingerprint with other fingerprints
+    """
+    metric_dict = {
+        "Tanimoto": oegraphsim.OETanimoto,
+        "Dice": oegraphsim.OEDice,
+        "Cosine": oegraphsim.OECosine,
+    }
+    if metric not in metric_dict:
+        raise ValueError(
+            f"Unsupported metric '{metric}'. Choose from {list(metric_dict.keys())}."
+        )
+
+    return [1 - metric_dict[metric](fp, x) for x in fps]
--- 1_import_data_process_fps/main_1.py
+++ 1_import_data_process_fps/main_1.py
@@ -1,13 +1,32 @@
-"""Create dataframes for the Industry Benchmark v1.2 and Discarded molecules from v1.1"""
+"""Create dataframes for the Industry Benchmark v1.2 and Discarded molecules from v1.1, output various distance
+matrixes.
+
+The major outputs of this module are in the outputs directory:
+
+- *.csv files
+    Containing unique CMILES and QCArchive entry names from the "OpenFF Industry Benchmark Season 1 v1.1"
+    for "OpenFF Industry Benchmark Season 1 v1.2" and removed entries.
+- distances_{Tanimoto | Dice | Cosine}_{Circular | MACCS | LINGO}.txt files
+    The first line contains the number of molecules and subsequent rows containing the upper triangle arrays for
+    "OpenFF Industry Benchmark Season 1 v1.2"
+- removed_distance_{Tanimoto | Dice | Cosine}_{Circular | MACCS | LINGO}.txt files
+    Each row contains the distance between that molecule and all molecules in the Industry Benchmark v1.2.
+
+"""
 
 from urllib.request import urlopen
 from loguru import logger
+import os
 
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 from qcportal import PortalClient
 
-from fp_utilities import get_fps
+from fp_utilities import get_fps, get_distance_matrix, get_distance_vector
+
+os.makedirs("outputs", exist_ok=True)
 
 # Pull Full Industry Dataset v1.1
 ADDRESS = "https://api.qcarchive.molssi.org:443/"
@@ -29,61 +48,117 @@ logger.info(
 )
 
 # Create dataframes for the cleaned Industry Dataset v1.2 and one for the removed entries:
-removed_entries = []
-final_entries = []
-for entry_name, spec_name, rec in ds.iterate_records():
-    if spec_name != "default":
-        continue
-    if rec.id in outlier_record_ids:
-        removed_entries.append(
-            {
-                "Name": entry_name,
-                "CMILES": rec.initial_molecule.extras[
-                    "canonical_isomeric_explicit_hydrogen_mapped_smiles"
-                ],
-            }
+filename_indbench = "outputs/industry_benchmark_v1.2_unique.csv"
+filename_removed = "outputs/removed_records_unique.csv"
+if not os.path.isfile(filename_indbench) or not os.path.isfile(filename_removed):
+    removed_entries = []
+    final_entries = []
+    for entry_name, spec_name, rec in ds.iterate_records():
+        if spec_name != "default":
+            continue
+        if rec.id in outlier_record_ids:
+            removed_entries.append(
+                {
+                    "Name": entry_name,
+                    "CMILES": rec.initial_molecule.extras[
+                        "canonical_isomeric_explicit_hydrogen_mapped_smiles"
+                    ],
+                }
+            )
+        else:
+            final_entries.append(
+                {
+                    "Name": entry_name,
+                    "CMILES": rec.initial_molecule.extras[
+                        "canonical_isomeric_explicit_hydrogen_mapped_smiles"
+                    ],
+                }
+            )
+    removed_df = pd.DataFrame(removed_entries)
+    final_df = pd.DataFrame(final_entries)
+
+    logger.info(
+        f"For conformers, there are {len(final_df)} in v1.2 and {len(removed_df)} removed."
+    )
+
+    # Consolidate rows with the same CMILES
+    removed_df = removed_df.groupby("CMILES", as_index=False).agg(
+        {"Name": lambda x: list(x)}
+    )
+    removed_df.rename(columns={"Name": "Names"}, inplace=True)
+
+    final_df = final_df.groupby("CMILES", as_index=False).agg(
+        {"Name": lambda x: list(x)}
+    )
+    final_df.rename(columns={"Name": "Names"}, inplace=True)
+    logger.info(
+        f"For unique molecules, there are {len(final_df)} in v1.2 and {len(removed_df)} removed."
+    )
+
+    removed_df.to_csv(filename_removed, index=False)
+    logger.info(f"Saved consolidated fingerprints '{filename_removed}'")
+    final_df.to_csv(filename_indbench, index=False)
+    logger.info(f"Saved consolidated fingerprints '{filename_indbench}'")
+else:
+    removed_df = pd.read_csv(filename_removed)
+    final_df = pd.read_csv(filename_indbench)
+    logger.info("Imported csv files")
+
+
+# Calculate distance matrices of final_molecules
+logger.info("Calculating distance matrices...")
+fig, axs = plt.subplots(1, 3, figsize=(8, 5), sharey=True)
+for j, fp_type in enumerate(["Circular", "MACCS", "LINGO"]):
+    logger.info(f"Getting {fp_type} fingerprints")
+    fps = get_fps(final_df["CMILES"].tolist(), fptype=fp_type)
+    for i, sim_type in enumerate(["Tanimoto", "Dice", "Cosine"]):
+        logger.info(f"Getting {sim_type} similarity distances")
+        filename = f"outputs/distances_{sim_type}_{fp_type}.txt"
+        if not os.path.isfile(filename):
+            distances = get_distance_matrix(fps, sim_type)  # dist_list, npts
+            logger.info(f"Saving 'outputs/distances_{sim_type}_{fp_type}.txt'")
+            with open(filename, "w") as f:
+                f.write(f"{distances[1]}\n")
+                for n in distances[0]:
+                    f.write(f"{n}\n")
+        else:
+            logger.info(f"Imported 'outputs/distances_{sim_type}_{fp_type}.txt'")
+            data = [
+                float(x.strip())
+                for x in open(filename).read().strip().split("\n")
+                if x != ""
+            ]
+            distances = (data[1:], int(data[0]))
+        # Plot similarity distribution
+        label = rf"{fp_type}: $\mu$ = {np.mean(distances[0]):0.2f} $\sigma$ = {np.std(distances[0]):0.2f}"
+        axs[i].hist(
+            distances[0],
+            density=True,
+            label=label,
+            alpha=0.6,
+            bins=30,
+            edgecolor="black",
+            linewidth=0.5,
         )
-    else:
-        final_entries.append(
-            {
-                "Name": entry_name,
-                "CMILES": rec.initial_molecule.extras[
-                    "canonical_isomeric_explicit_hydrogen_mapped_smiles"
-                ],
-            }
+        if j == 0:
+            if i == 0:
+                axs[0].set_ylabel("Probability")
+            axs[i].set_xlabel(f"{sim_type} Distance")
+        axs[i].legend(loc="best")
+        axs[i].set_xlim((0, 1))
+
+plt.tight_layout()
+fig.savefig("outputs/distance_distributions.png", dpi=600)
+plt.close(fig)
+
+
+# Save distances of removed molecules from final molecules
+for fp_type in ["Circular", "MACCS", "LINGO"]:
+    fps = get_fps(removed_df["CMILES"].tolist(), fptype=fp_type)
+    fps_final = get_fps(final_df["CMILES"].tolist(), fptype=fp_type)
+    for i, sim_type in enumerate(["Tanimoto", "Dice", "Cosine"]):
+        rec_distances = [get_distance_vector(x, fps_final, sim_type) for x in fps]
+        open(f"outputs/removed_distance_{sim_type}_{fp_type}.txt", "w").write(
+            "\n".join([", ".join([str(y) for y in x]) for x in rec_distances])
         )
-removed_df = pd.DataFrame(removed_entries)
-final_df = pd.DataFrame(final_entries)
-
-logger.info(
-    f"For conformers, there are {len(final_df)} in v1.2 and {len(removed_df)} removed."
-)
-
-
-# Consolidate rows with the same CMILES
-removed_df = removed_df.groupby("CMILES", as_index=False).agg(
-    {"Name": lambda x: list(x)}
-)
-removed_df.rename(columns={"Name": "Names"}, inplace=True)
-
-final_df = final_df.groupby("CMILES", as_index=False).agg({"Name": lambda x: list(x)})
-final_df.rename(columns={"Name": "Names"}, inplace=True)
-logger.info(
-    f"For unique molecules, there are {len(final_df)} in v1.2 and {len(removed_df)} removed."
-)
-
-
-# Calculate OE fingerprints for the molecules:
-final_df["Circular FP"] = get_fps(final_df["CMILES"].tolist(), fptype="Circular")
-final_df["MACCS FP"] = get_fps(final_df["CMILES"].tolist(), fptype="MACCS")
-final_df["LINGO FP"] = get_fps(final_df["CMILES"].tolist(), fptype="LINGO")
-
-removed_df["Circular FP"] = get_fps(removed_df["CMILES"].tolist(), fptype="Circular")
-removed_df["MACCS FP"] = get_fps(removed_df["CMILES"].tolist(), fptype="MACCS")
-removed_df["LINGO FP"] = get_fps(removed_df["CMILES"].tolist(), fptype="LINGO")
-logger.info("Added Circular, MACCS, and LINGO fingerprints for each CMILES string.")
-
-removed_df.to_csv("removed_records_unique_fps.csv", index=False)
-logger.info("Saved consolidated fingerprints 'removed_record_unique_fps.csv'")
-final_df.to_csv("industry_benchmark_v1.2_unique_fps.csv", index=False)
-logger.info("Saved consolidated fingerprints 'industry_benchmark_v1.2_unique_fps.csv'")
+        logger.info(f"Saved 'outputs/removed_distance_{sim_type}_{fp_type}.txt'")
